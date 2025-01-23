9330648c57b60bca783f313bcf432159e5a88dec
1YousefHany <yousefsprog1@gmail.com>
2025-01-23 15:02:46 +0200

    creating some Custom things & handle getNewsStates Done

---
+++ lib/widgets/custom_circular_indicator.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../helper/constants.dart';
+
+class CustomCircularIndicator extends StatelessWidget {
+  const CustomCircularIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverToBoxAdapter(
+      child:  SizedBox(
+        height: 450,
+        child: Center(
+          child:  CircularProgressIndicator(
+           strokeWidth: 5,
+            color: kPrimaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
